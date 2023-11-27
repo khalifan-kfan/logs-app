31317b0adbc422095cb50771461ad2af02451d89
khalifan-kfan <khalifanmuwonge@gmail.com>
2023-11-27 18:24:14 +0300

    enable logs storage

--- controllers/logHandlers.go
+++ controllers/logHandlers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/khalifan-kfan/logs-app/config"
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,8 +29,18 @@ func HandleLogs(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Key: %s, Value: %v\n", key, value)
 	}
 
+	client := config.GetClient()
+
+	collection := client.Database("logs").Collection("fluentdLogs")
+
+	_, err := collection.InsertOne(r.Context(), data)
+	if err != nil {
+		http.Error(w, "Failed to store logs", http.StatusInternalServerError)
+		return
+	}
+
 	// Prepare a response
-	response := map[string]string{"message": "Received values successfully"}
+	response := map[string]string{"message": "Received values successfully and stored logs"}
 
 	// Encode the response to JSON and send it back
 	sendJSONResponse(w, http.StatusOK, response)
